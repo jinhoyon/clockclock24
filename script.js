1bbab05bc7e144898484171b993aa39406cb0f35
Jin Ho Yon <jyon@ucdavis.edu>
2025-05-16 00:09:45 -0700

    finish.

--- script.js
+++ script.js
@@ -1,23 +1,13 @@
+// Columns
+const columnOne = document.querySelector("#column_one");
+const columnTwo = document.querySelector("#column_two");
+const columnThree = document.querySelector("#column_three");
+const columnFour = document.querySelector("#column_four");
+
+let clockOne, clockTwo, clockThree;
+let hourHand, minuteHand;
+
 (function () {
-    // Current Date
-    const now = new Date();
-
-    const hoursNow = now.getHours() % 12 || 12;
-    const minutesNow = now.getMinutes();
-
-    // Columns
-    const columnOne = document.querySelector("#column_one");
-    const columnTwo = document.querySelector("#column_two");
-    const columnThree = document.querySelector("#column_three");
-    const columnFour = document.querySelector("#column_four");
-    const columnFive = document.querySelector("#column_five");
-    const columnSix = document.querySelector("#column_six");
-    const columnSeven = document.querySelector("#column_seven");
-    const columnEight = document.querySelector("#column_eight");
-
-    let clockOne, clockTwo, clockThree;
-    let hourHand, minuteHand;
-
     // helper to set clocks from column
     function setClocks(column) {
         clockOne = column.querySelector(".clock_one");
@@ -30,8 +20,8 @@
 
     // helper to set a clock's hands
     function setClockHands(clock, hourDegrees, minuteDegrees) {
-        hourHand = clock.querySelector('.hour_hand');
-        minuteHand = clock.querySelector('.minute_hand');
+        hourHand = clock.querySelector(".hour_hand");
+        minuteHand = clock.querySelector(".minute_hand");
         hourHand.style.transform = `rotate(${hourDegrees}deg)`;
         minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
     }
@@ -52,7 +42,7 @@
         setClockHands(clockOne, 135, 135);
         setClockHands(clockThree, 135, 135);
         setClockHands(clockFive, 135, 135);
-        
+
         setClockHands(clockTwo, 90, 90);
         setClockHands(clockFour, 90, 270);
         setClockHands(clockSix, 270, 270);
@@ -146,58 +136,114 @@
         setClockHands(clockSix, 180, 270);
     }
 
-    switch (hoursNow) {
-        case 1:
-            setZero(columnOne);
-            setOne(columnTwo);
-            break;
-        case 2:
-            setZero(columnOne);
-            setTwo(columnTwo);
-            break;
-        case 3:
-            setZero(columnOne);
-            setThree(columnTwo);
-
-            break;
-        case 4:
-            setZero(columnOne);
-            setFour(columnTwo);
-            break;
-        case 5:
-            setZero(columnOne);
-            setFive(columnTwo);
-            break;
-        case 6:
-            setZero(columnOne);
-            setSix(columnTwo);
-            break;
-        case 7:
-            setZero(columnOne);
-            setSeven(columnTwo);
-            break;
-        case 8:
-            setZero(columnOne);
-            setEight(columnTwo);
-            break;
-        case 9:
-            setZero(columnOne);
-            setNine(columnTwo);
-            break;
-        case 10:
-            setOne(columnOne);
-            setZero(columnTwo);
-            break;
-        case 11:
-            setOne(columnOne);
-            setOne(columnTwo);
-            break;
-        case 12:
-            setOne(columnOne);
-            setTwo(columnTwo);
-            break;
-        default:
+    function setHours() {
+        const currentHour = new Date().getHours() % 12 || 12;
+        switch (currentHour) {
+            case 1:
+                setZero(columnOne);
+                setOne(columnTwo);
+                break;
+            case 2:
+                setZero(columnOne);
+                setTwo(columnTwo);
+                break;
+            case 3:
+                setZero(columnOne);
+                setThree(columnTwo);
+                break;
+            case 4:
+                setZero(columnOne);
+                setFour(columnTwo);
+                break;
+            case 5:
+                setZero(columnOne);
+                setFive(columnTwo);
+                break;
+            case 6:
+                setZero(columnOne);
+                setSix(columnTwo);
+                break;
+            case 7:
+                setZero(columnOne);
+                setSeven(columnTwo);
+                break;
+            case 8:
+                setZero(columnOne);
+                setEight(columnTwo);
+                break;
+            case 9:
+                setZero(columnOne);
+                setNine(columnTwo);
+                break;
+            case 10:
+                setOne(columnOne);
+                setZero(columnTwo);
+                break;
+            case 11:
+                setOne(columnOne);
+                setOne(columnTwo);
+                break;
+            case 12:
+                setOne(columnOne);
+                setTwo(columnTwo);
+                break;
+            default:
+        }
     }
 
-    // console.log(`Current time: ${hoursNow}:${minutesNow}`);
+    function setMinutes(digit, column) {
+        switch (digit) {
+            case 0:
+                setZero(column);
+                break;
+            case 1:
+                setOne(column);
+                break;
+            case 2:
+                setTwo(column);
+                break;
+            case 3:
+                setThree(column);
+                break;
+            case 4:
+                setFour(column);
+                break;
+            case 5:
+                setFive(column);
+                break;
+            case 6:
+                setSix(column)
+                break;
+            case 7:
+                setSeven(column);
+                break;
+            case 8:
+                setEight(column);
+                break;
+            case 9:
+                setNine(column);
+                break;
+            default:
+        }
+    }
+
+    function updateTime() {
+        const now = new Date();
+        const currentHour = now.getHours() % 12 || 12;
+        const currentMinute = now.getMinutes();
+        const currentMinuteTensDigit = Math.floor(currentMinute / 10);
+        const currentMinuteOnesDigit = currentMinute % 10;
+
+        setHours();
+        setMinutes(currentMinuteTensDigit, columnThree);
+        setMinutes(currentMinuteOnesDigit, columnFour);
+    }
+
+    // Initial call
+    updateTime();
+
+    // Update every second
+    setInterval(updateTime, 1000);
 })();
+
+
